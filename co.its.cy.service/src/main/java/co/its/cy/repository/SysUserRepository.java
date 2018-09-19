c0741052d894f887a1bf71f0292bd340a0fb820c
a985890777 <985890777@qq.com>
2018-09-19 10:12:06 +0800

    合并

--- co.its.cy.service/src/main/java/co/its/cy/repository/SysUserRepository.java
+++ co.its.cy.service/src/main/java/co/its/cy/repository/SysUserRepository.java
@@ -1,7 +1,8 @@
 package co.its.cy.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import co.its.cy.entity.SysUser;
 public interface SysUserRepository   extends JpaRepository<SysUser, Integer>{
-
+	SysUser findByUserName(String userName);
 }
--- co.its.cy.web/src/main/java/co/its/cy/web/open/controller/sys/SysUserController.java
+++ co.its.cy.web/src/main/java/co/its/cy/web/open/controller/sys/SysUserController.java
@@ -1,12 +1,38 @@
 package co.its.cy.web.open.controller.sys;
 
+import java.io.Serializable;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController(value="/sys")
+import com.alibaba.dubbo.config.annotation.Reference;
+
+import co.its.cy.api.sys.SysUserService;
+import co.its.cy.web.open.common.entity.ApiResult;
+import co.its.cy.web.open.common.entity.ApiResultGenerator;
+
+@RestController
 @ResponseBody
 public class SysUserController {
+    @Autowired
+	private RedisTemplate<Object, Object> redisTemplate;
     
+    @Reference
+    private SysUserService sysUserService;
 	
+	@RequestMapping(value="/login")
+	public ApiResult login(String action,String jsonString,HttpServletRequest request) {
+		Serializable user =  sysUserService.login(jsonString);
+	/*	Boolean flag = redisTemplate.opsForValue().setBit("login"+ DateUtil.formatDate(new Date()),jsonObject.getLong("id") , true);
+		if(flag) {
+			ApiResultGenerator.successRessult(flag);
+		}*/
+		return ApiResultGenerator.result(true, "登陆失败", "", "", 0, null);
+	}
 	
 }
